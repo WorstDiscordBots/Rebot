1024b3e13061cbbe7b10c9f917bbc2d3e741ba31
diniboy <Diniboy1123@users.noreply.github.com>
2018-10-20 00:12:31 +0200

    Added global utils (#5)

---
+++ utils.py
@@ -0,0 +1,20 @@
+"""
+    Rebot - A random Discord bot that helps mobile users to use desktop client built-in commands
+    Copyright (C) 2018  Diniboy1123 <diniboy [at] protonmail [dot] com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+def remove_mentions(content: str):
+    return content.replace("@", "@\N{ZERO WIDTH SPACE}")
